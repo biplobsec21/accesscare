e91314803c89cacac2ced1d724d87a5ae56cbd3c
Vac1911 <TnMI6&2@61I8699t5jw%OJm08XJZSf>
2019-08-19 10:24:19 -0400

    blacklist_debug

--- config/app.php
+++ config/app.php
@@ -227,5 +227,26 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
     ],
+    'debug_blacklist' => [
+        '_ENV' => [
+            'APP_KEY',
+            'DB_PASSWORD',
+            'REDIS_PASSWORD',
+            'MAIL_PASSWORD',
+            'PUSHER_APP_KEY',
+            'PUSHER_APP_SECRET',
+        ],
+        '_SERVER' => [
+            'APP_KEY',
+            'DB_PASSWORD',
+            'REDIS_PASSWORD',
+            'MAIL_PASSWORD',
+            'PUSHER_APP_KEY',
+            'PUSHER_APP_SECRET',
+        ],
+        '_POST' => [
+            'password',
+        ],
+    ],
 
 ];
